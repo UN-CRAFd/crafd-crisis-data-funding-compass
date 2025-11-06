56857654ca900624d70c5042a366f71a4dcee6be
leonhenrik <106387041+leonhenrik@users.noreply.github.com>
2025-11-06 14:33:23 -0500

    logo update

--- src/types/airtable.ts
+++ src/types/airtable.ts
@@ -63,6 +63,7 @@ export interface ProjectData {
     isCrafdFunded: boolean | string;
     provider: string;
     productKey: string;
+    hdxSohd?: string;
 }
 
 export interface OrganizationWithProjects {
