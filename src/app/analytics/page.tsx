d89c990f74ca1880492a8a748d79a9f354e2c326
leonhenrik <106387041+leonhenrik@users.noreply.github.com>
2025-12-18 19:17:08 -0500

    build fixed

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -5,6 +5,8 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Analytics() {
     return (
         <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
