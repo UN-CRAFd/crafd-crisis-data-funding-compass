16bbfaedfa5a83251f429faf32e5726a001a90df
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-12-26 15:04:24 -0500

    upgrade to next.js 16

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,6 @@ const nextConfig = {
     unoptimized: true
   },
   serverExternalPackages: ['d3-org-chart', 'd3'],
-  eslint: {
-    // FIXME: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
